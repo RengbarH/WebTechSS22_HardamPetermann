25e84fceff18cd6e141d59b0e26c07434f7653ae
RengbarH <86048645+RengbarH@users.noreply.github.com>
2022-04-19 19:15:54 +0200

    assert Test DisplayName

--- src/main/java/de/htwberlin/HelloWorldController.java
+++ src/main/java/de/htwberlin/HelloWorldController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class controller {
+public class HelloWorldController {
 
     @GetMapping(value = "/")
-    public String index(){
+    public String index() {
         return "helloWorld";
     }
 }
--- src/test/java/de/htwberlin/DebtCalculationApplicationTests.java
+++ src/test/java/de/htwberlin/DebtCalculationApplicationTests.java
@@ -1,5 +1,6 @@
 package de.htwberlin;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DebtCalculationApplicationTests {
 
 	@Test
+	@DisplayName("Test1")
 	void contextLoads() {
 	}
 
