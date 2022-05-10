c73da617b3f7f6bafb46bb7e514c8f7e649fcbda
PetermannTobias <saibot.96@gmx.net>
2022-05-10 16:46:42 +0200

    Klasse PersonRestController aktualisiert

--- src/main/java/de/htwberlin/web/PersonRestController.java
+++ src/main/java/de/htwberlin/web/PersonRestController.java
@@ -1,13 +1,52 @@
 package de.htwberlin.web;
 
+import de.htwberlin.service.PersonService;
 import de.htwberlin.web.api.Person;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import de.htwberlin.web.api.PersonManipulationRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
 public class PersonRestController {
 
+    private final PersonService personService;
+
+    public PersonRestController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping(path = "/api/v1/persons")
+    public ResponseEntity<List<Person>> fetchPersons() {
+        return ResponseEntity.ok(personService.findAll());
+    }
+
+    @GetMapping(path = "/api/v1/persons/{id}")
+    public ResponseEntity<Person> fetchPersonById(@PathVariable long id){
+        var person = personService.findById(id);
+        return person != null? ResponseEntity.ok(person): ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(path = "/api/v1/persons")
+    public ResponseEntity<Void> createPerson(@RequestBody PersonManipulationRequest request) throws URISyntaxException {
+        var person = personService.create(request);
+        URI uri = new URI("/api/v1/persons/" + person.getId());
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(path = "/api/v1/persons/{id}")
+    public ResponseEntity<Person> updatePerson(@PathVariable Long id, @RequestBody PersonManipulationRequest request) {
+        var person = personService.update(id, request);
+        return person != null? ResponseEntity.ok(person): ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping(path = "/api/v1/persons/{id}")
+    public ResponseEntity<Void> deletePerson(@PathVariable Long id) {
+        boolean successful = personService.deleteById(id);
+        return successful? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
+    }
 
 }
