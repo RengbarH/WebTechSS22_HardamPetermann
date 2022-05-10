31a96d1576bfabfe9ffee59558b9f3dcb3f11c66
PetermannTobias <saibot.96@gmx.net>
2022-05-10 16:33:14 +0200

    Klasse PersonRestController erstellt

--- src/main/java/de/htwberlin/web/PersonRestController.java
+++ src/main/java/de/htwberlin/web/PersonRestController.java
@@ -1,4 +1,13 @@
 package de.htwberlin.web;
 
+import de.htwberlin.web.api.Person;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class PersonRestController {
+
+
 }
