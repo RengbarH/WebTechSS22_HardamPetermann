6b14a29db3193c8678aa53dc51cca7e6f4f61987
PetermannTobias <saibot.96@gmx.net>
2022-05-10 17:26:05 +0200

    Klasse application erstellt

--- src/main/java/de/htwberlin/service/PersonService.java
+++ src/main/java/de/htwberlin/service/PersonService.java
@@ -64,7 +64,7 @@ public class PersonService {
                 personEntity.getId(),
                 personEntity.getFirstname(),
                 personEntity.getLastname(),
-                personEntity.isVaccinated()
+                personEntity.isDebts()
         );
     }
 }
